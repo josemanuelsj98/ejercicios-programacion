4d05d70c1fbcf2a28c381609fc00a0bf9d9add8f
José Manuel Suárez Jiménez <josemanuelsj98@gmail.com>
2017-10-31 08:48:18 +0100

    Add files via upload

---
+++ ejercicios-master/ejercicios-master/ejercicios.git/Ejercicios/tema1/ejercicio2.java
@@ -0,0 +1,8 @@
+public class ejercicio2 {
+  public static void main(String[] args) {
+   System.out.println("José Manuel");
+   System.out.println("640345698");
+   System.out.println("El holandés errante");
+   
+  }
+}
---
+++ ejercicios-master/ejercicios-master/ejercicios.git/Ejercicios/tema2/ejercicio3.java
@@ -0,0 +1,14 @@
+public class ejercicio3 {
+  public static void main(String[] args) {
+    String nombre,direccion;
+    int telefono;
+    nombre="José Manuel";
+    direccion="Calle José Calderón";
+    telefono=640277658
+    
+    System.out.println("Mi nombre es " + nombre);
+    System.out.println("Mi calle es " + direccion);
+    System.out.println("Mi telefono es " + telefono);
+   
+  }
+}
