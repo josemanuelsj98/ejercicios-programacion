6186769d708767edbd01576189e09a0f043a679f
José Manuel Suárez Jiménez <josemanuelsj98@gmail.com>
2017-10-31 08:43:31 +0100

    Add files via upload

---
+++ ejercicios-master/ejercicios.git/Ejercicios/tema4/Ejercicio18.java
@@ -0,0 +1,26 @@
+/**
+ * ultima cifra
+ *
+ * @author José Manuel Suárez Jiménez
+ */
+
+public class Ejercicio18 {
+  public static void main(String[] args) {
+    System.out.println("Introduce un numero entero para saber su primera cifra");
+    int a = Integer.parseInt(System.console() .readLine());
+    a=a%1;
+    System.out.println(a%1);
+    
+    
+    
+      
+      
+      
+    
+    
+  
+       
+    
+   
+  }
+}
